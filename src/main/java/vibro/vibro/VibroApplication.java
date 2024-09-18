3a943a58bee0cb5f63d9d59e33f23e77084495fd
Nikolay Ivanov <niki.evgeniev@gmail.com>
2024-09-18 23:32:42 +0300

    Repo init

---
+++ src/main/java/vibro/vibro/VibroApplication.java
@@ -0,0 +1,13 @@
+package vibro.vibro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VibroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vibro/vibro/VibroApplicationTests.java
@@ -0,0 +1,13 @@
+package vibro.vibro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
