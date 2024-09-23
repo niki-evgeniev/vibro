3ff46c1f46f569d045df7ad43049e7c6591f3db7
Nikolay Ivanov <niki.evgeniev@gmail.com>
2024-09-24 00:48:47 +0300

    Add contact page

--- src/main/java/vibro/vibro/Controller/ContactsController.java
+++ src/main/java/vibro/vibro/Controller/ContactsController.java
@@ -1,17 +1,16 @@
 package vibro.vibro.Controller;
 
-
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class ProductController {
+public class ContactsController {
 
-    @GetMapping("/products")
+    @GetMapping("/contacts")
     public ModelAndView products(HttpServletRequest request) {
-        ModelAndView modelAndView = new ModelAndView("products");
+        ModelAndView modelAndView = new ModelAndView("contact");
         modelAndView.addObject("currentUrl", request.getRequestURI());
         return modelAndView;
     }
